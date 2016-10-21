c0507d64806738560e1a9c316739fa620af3f6e1
kehra <myoke.k@gmail.com>
2016-10-21 16:48:38 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("main").style.width = "90%";
+let articles = document.getElementsByClassName("l-doubleColumn-main");
+if (articles.length > 0) {
+  articles[0].style.width = "80%";
+}
