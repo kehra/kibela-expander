1c295721e82a6894e031e76e885d4ccca638540a
kehra <myoke.k@gmail.com>
2016-10-26 17:23:12 +0900

    minWidthを追加

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-document.getElementById("main").style.width = "90%";
+let main = document.getElementById("main");
+if (main) {
+  main.style.width = "90%";
+  main.style.minWidth = "1180px";
+}
 let articles = document.getElementsByClassName("l-doubleColumn-main");
 if (articles.length > 0) {
   articles[0].style.width = "80%";
+  articles[0].style.minWidth = "800px";
 }
